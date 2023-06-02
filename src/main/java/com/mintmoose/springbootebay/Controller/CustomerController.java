38ca747896e15fb9a4fd6f6d206f4a70d36c50ca
FunkyChicken64 <riqab@hotmail.co.uk>
2023-06-02 23:47:14 +0100

    CRUD simply implemented

--- src/main/java/com/mintmoose/springbootebay/Controller/CustomerController.java
+++ src/main/java/com/mintmoose/springbootebay/Controller/CustomerController.java
@@ -1,11 +1,10 @@
 package com.mintmoose.springbootebay.Controller;
 
 import com.mintmoose.springbootebay.Model.Customer;
+import com.mintmoose.springbootebay.Model.NewCustomerRequest;
 import com.mintmoose.springbootebay.Service.CustomerService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +15,22 @@ public class CustomerController {
 
     private CustomerService customerService;
 
-    @GetMapping
+    @GetMapping("customer")
     public List<Customer> getAllCustomer() {
         return customerService.getAllCustomers();
+    }
+
+    @PostMapping("sign-up")
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        customerService.createCustomer(request);
+    }
+
+    @DeleteMapping("customer/{customerId}")
+    public void deleteCustomer(@PathVariable Long customerId) {
+        customerService.deleteCustomer(customerId);
+    }
+
+    public void updateCustomer(@PathVariable Long customerId, @RequestBody NewCustomerRequest request) {
+        customerService.updateCustomer(customerId, request);
     }
 }
---
+++ src/main/java/com/mintmoose/springbootebay/Model/NewCustomerRequest.java
@@ -0,0 +1,3 @@
+package com.mintmoose.springbootebay.Model;
+
+public record NewCustomerRequest(String firstName, String lastName, String email, String password) {}
