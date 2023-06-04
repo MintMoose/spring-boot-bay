deaae28fd9ca60e029317967ec909df939aa906a
FunkyChicken64 <riqab@hotmail.co.uk>
2023-06-04 13:53:06 +0100

    Create JWTService and implemented all methods

--- src/main/java/com/mintmoose/springbootebay/Controller/CustomerController.java
+++ src/main/java/com/mintmoose/springbootebay/Controller/CustomerController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 public class CustomerController {
 
-    private CustomerService customerService;
+    private final CustomerService customerService;
 
     @GetMapping("customer")
     public List<Customer> getAllCustomer() {
