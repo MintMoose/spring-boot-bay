2cfeb9621364c647331575dc26f6598010aeea9f
FunkyChicken64 <riqab@hotmail.co.uk>
2023-06-14 10:45:30 +0100

    implement Profile page.

--- frontend/eshop/src/components/Profile.js
+++ frontend/eshop/src/components/Profile.js
@@ -1,26 +1,57 @@
 import React from "react";
-import api from "../api/axiosConfig";
-import { useState, useEffect } from "react";
+import ProductCard from "./product/ProductCard";
+import "./Profile.css";
+function Profile({ authData, userProducts }) {
+  return (
+    <div className="profile-container">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+            Active
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">
+            Disabled
+          </a>
+        </li>
+      </ul>
+      <div className="profile-inner">
+        <h1> {authData.username}</h1>
+        <div>
+          <h3>Information:</h3>
+        </div>
 
-function Profile({ authData }) {
-  const [customer, setCustomer] = useState();
-  const fetchCustomer = async () => {
-    try {
-      const response = await api.get(`/customers/${authData.username}`);
-      setCustomer(response.data.username);
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-    }
-  };
-
-  useEffect(() => {
-    if (authData.username) {
-      fetchCustomer();
-    }
-  }, [authData.username]);
-
-  return <div>Profile, {customer}</div>;
+        <div>
+          <h3>My Products:</h3>
+          <div className="product-list2">
+            {userProducts && userProducts.length > 0 ? (
+              userProducts.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  username={authData.username}
+                  size="small"
+                />
+              ))
+            ) : (
+              <p>No products found.</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
