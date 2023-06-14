7d46c872b5286d9b9a4166ae88a1b2aeea7b68b2
FunkyChicken64 <riqab@hotmail.co.uk>
2023-06-14 10:45:48 +0100

    Fake loading to Layout page.

--- frontend/eshop/src/components/Layout/Layout.js
+++ frontend/eshop/src/components/Layout/Layout.js
@@ -1,15 +1,42 @@
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Layout.css";
 import Navbar from "./Navbar";
 
 import React from "react";
 
 const Layout = ({ authData }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="final">
       <main>
-        <Navbar isLoggedIn={authData.isLoggedIn} />
-        <Outlet />
+        <div>
+          {isLoading ? (
+            <div className="loading">
+              <div
+                class="spinner-border"
+                style={{ width: "3rem", height: "3rem" }}
+                role="status"
+              >
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <Navbar isLoggedIn={authData.isLoggedIn} />
+              <Outlet />
+            </div>
+          )}
+        </div>
       </main>
       <footer className="footer">
         <p>© 2022-2023, Springbay, Inc. or its affiliates </p>
