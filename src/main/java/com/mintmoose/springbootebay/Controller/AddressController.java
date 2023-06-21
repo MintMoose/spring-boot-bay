476867654e1cf494e79c8e28625dbe4be465fb49
FunkyChicken64 <riqab@hotmail.co.uk>
2023-06-21 23:24:01 +0100

    Create address post request for authenticated users using their token for customer id

--- src/main/java/com/mintmoose/springbootebay/Controller/AddressController.java
+++ src/main/java/com/mintmoose/springbootebay/Controller/AddressController.java
@@ -1,6 +1,9 @@
 package com.mintmoose.springbootebay.Controller;
 
+import com.mintmoose.springbootebay.Model.Address;
+import com.mintmoose.springbootebay.Model.NewAddressRequest;
 import com.mintmoose.springbootebay.Model.Customer;
+import com.mintmoose.springbootebay.Model.Product;
 import com.mintmoose.springbootebay.Service.AddressService;
 import com.mintmoose.springbootebay.Service.CustomerService;
 import lombok.AllArgsConstructor;
@@ -45,13 +48,27 @@ public class AddressController {
     public ResponseEntity<?> getAddressByCustomerId(@PathVariable("customerId") Long customerId, Authentication authentication) {
         if (authentication.isAuthenticated()) {
             String username = authentication.getName();
-            Customer requestCustomer = customerService.getCustomerByUsername(username)
-                    .orElseThrow(() -> new IllegalArgumentException("Access denied. Invalid authorization."));
             Customer variableCustomer = customerService.getCustomerById(customerId).orElseThrow(() -> new IllegalArgumentException("Id doesn't match!"));
             try {
                 if (Objects.equals(variableCustomer.getUsername(), username)) {
                     return ResponseEntity.ok(addressService.getAddressByCustomerId(customerId));
                 }
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Access denied. Invalid authorization.");
+
+    }
+
+    @PostMapping("/{customerId}")
+    public ResponseEntity<?> createAddress(@RequestBody NewAddressRequest request, Authentication authentication) {
+        if (authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            Customer variableCustomer = customerService.getCustomerByUsername(username).orElseThrow(() -> new IllegalArgumentException("Id doesn't match!"));
+            try {
+                    Address createdAddress = addressService.createAddress(request);
+                    return ResponseEntity.status(HttpStatus.CREATED).body(createdAddress);
             } catch (IllegalArgumentException e) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
             }
