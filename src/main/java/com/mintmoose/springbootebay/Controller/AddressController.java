4e9de2a30391b4c1ad4a39b9b06c648663ba857e
FunkyChicken64 <riqab@hotmail.co.uk>
2023-06-21 22:53:24 +0100

    authentication address logic for users.

--- src/main/java/com/mintmoose/springbootebay/Controller/AddressController.java
+++ src/main/java/com/mintmoose/springbootebay/Controller/AddressController.java
@@ -1,10 +1,15 @@
 package com.mintmoose.springbootebay.Controller;
 
+import com.mintmoose.springbootebay.Model.Customer;
 import com.mintmoose.springbootebay.Service.AddressService;
 import com.mintmoose.springbootebay.Service.CustomerService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/address")
@@ -15,18 +20,43 @@ public class AddressController {
     private final AddressService addressService;
     private final CustomerService customerService;
 
-    @GetMapping()
-    public ResponseEntity<?> getAllAddress() {
-        return ResponseEntity.ok(addressService.getAllAddresses());
-    }
 
     @GetMapping("/{Id}")
-    public ResponseEntity<?> getAddressById(@PathVariable("Id") Long id) {
+    public ResponseEntity<?> getAddressById(@PathVariable("Id") Long id, Authentication authentication) {
+        Long customerId = addressService.getAddressById(id).getCustomerId();
+        if (authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            Customer requestCustomer = customerService.getCustomerByUsername(username)
+                    .orElseThrow(() -> new IllegalArgumentException("Access denied. Invalid authorization."));
+            Customer idCustomer = customerService.getCustomerById(customerId)
+                    .orElseThrow( () -> new IllegalArgumentException("Id is wrong"));
+            try {
+                if (Objects.equals(requestCustomer, idCustomer)) {
+                    return ResponseEntity.ok(addressService.getAddressById(id));
+                }
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            }
+        }
         return ResponseEntity.ok(addressService.getAddressById(id));
     }
 
     @GetMapping("/{customerId}")
-    public ResponseEntity<?> getAddressByCustomerId(@PathVariable("customerId") Long customerId) {
-        return ResponseEntity.ok(addressService.getAddressByCustomerId(customerId));
+    public ResponseEntity<?> getAddressByCustomerId(@PathVariable("customerId") Long customerId, Authentication authentication) {
+        if (authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            Customer requestCustomer = customerService.getCustomerByUsername(username)
+                    .orElseThrow(() -> new IllegalArgumentException("Access denied. Invalid authorization."));
+            Customer variableCustomer = customerService.getCustomerById(customerId).orElseThrow(() -> new IllegalArgumentException("Id doesn't match!"));
+            try {
+                if (Objects.equals(variableCustomer.getUsername(), username)) {
+                    return ResponseEntity.ok(addressService.getAddressByCustomerId(customerId));
+                }
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Access denied. Invalid authorization.");
+
     }
 }
