eabb0590ce196e9f05058c6342ea013628cce93d
FunkyChicken64 <riqab@hotmail.co.uk>
2023-06-21 22:55:26 +0100

    Created admin address endpoint.

---
+++ src/main/java/com/mintmoose/springbootebay/Controller/AdminAddressController.java
@@ -0,0 +1,23 @@
+package com.mintmoose.springbootebay.Controller;
+
+import com.mintmoose.springbootebay.Service.AddressService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/address")
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class AdminAddressController {
+
+    private final AddressService addressService;
+
+    @GetMapping()
+    public ResponseEntity<?> getAllAddress() {
+        return ResponseEntity.ok(addressService.getAllAddresses());
+    }
+}
