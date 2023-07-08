e26cb7825e1124a6e03b65278fdc84c5a79f8ca1
FunkyChicken64 <riqab@hotmail.co.uk>
2023-07-08 19:39:58 +0100

    Button to direct to create product page

--- frontend/eshop/src/profilePage/tabs/MyProducts.js
+++ frontend/eshop/src/profilePage/tabs/MyProducts.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import ProductCard from "../../components/product/ProductCard";
 
 function MyProducts({ userProducts, setUserProducts, setProducts, authData }) {
+  const navigate = useNavigate();
+
+  const handleCreateProduct = () => {
+    navigate("/product/create");
+  };
+
   return (
     <Container>
       <Row>
         <Col>
+          <div className="create-product-div">
+            <button onClick={handleCreateProduct}>Create Product</button>
+          </div>
           <h1>My Products</h1>
           <div className="product-list">
             {userProducts && userProducts.length > 0 ? (
