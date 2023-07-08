ef68ceeb932467ad1db13937ef4755fb12165766
FunkyChicken64 <riqab@hotmail.co.uk>
2023-07-08 19:39:02 +0100

    React Create context use. not applied properly. Its suppose to refresh page when state change.

--- frontend/eshop/src/index.js
+++ frontend/eshop/src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProductProvider } from "./components/product/ProductContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />}></Route>
-      </Routes>
+      <ProductProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </ProductProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
